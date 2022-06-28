5e468113298da2e87be8abfb1f46feecea9c181a
GustavoArcoverde <garcoverde3@gmail.com>
2022-06-28 13:12:23 -0300

    cria tabela garçom em database.db

--- src/infra/create-table.js
+++ src/infra/create-table.js
@@ -2,3 +2,22 @@ import sqlite3 from 'sqlite3';
 export const db = new sqlite3.Database('./database.db');
 
 // Quem quiser, pode usar esse arquivo pra criar as tabelas das entidades, aí na hora do merge a gente coloca pra deixar tudo, e nenhuma alteração se perde :D
+
+const tabela_garcom = `
+CREATE TABLE IF NOT EXISTS "GARÇOM"(
+    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "NOME" varchar(64),
+    "CPF" varchar(11),
+    "TELEFONE" varchar(11),
+    "TURNO" varchar(7),
+    "PRAÇA" tinyint,
+    "COMISSÃO" varchar(3)
+)`
+
+function cria_tabela_garcom(){
+    db.run(tabela_garcom, (error)=>{
+        if(error) console.log("Erro ao criar tabela garçom")
+    })
+}
+
+cria_tabela_garcom()
