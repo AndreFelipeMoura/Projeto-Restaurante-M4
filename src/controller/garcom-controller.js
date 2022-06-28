cfcd5078810bd8f3806e33b4c1814e0054b63209
GustavoArcoverde <garcoverde3@gmail.com>
2022-06-28 13:40:15 -0300

    importa garçom controller no index e cria app.listen

--- src/controller/garcom-controller.js
+++ src/controller/garcom-controller.js
@@ -3,5 +3,7 @@ import {bd} from '../infra/sqlite-bd.js'
 import {GarçomDAO} from '../DAO/garcom-dao.js'
 
 export const garçom = (app) =>{
-    
+const newGarcomDAO = new GarçomDAO(bd)
+
+    app.get("./garcom")
 }
--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000;
 app.use(express.json())
+
+import {garçom} from './controller/garcom-controller'
+garçom(app)
+
+app.listen(port, ()=>{
+    console.log("Ta rodando meu consagrado")
+})
