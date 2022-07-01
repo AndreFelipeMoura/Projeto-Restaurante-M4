b1006eb9936d4f72202c960d1090cfdb09d72252
Orlando C Bernardi <orlandoceciliobernardi2017@gmail.com>
2022-07-01 16:35:52 -0300

    model cliente

---
+++ src/models/cliente-models.js
@@ -0,0 +1,10 @@
+class clientemodels{
+    constructor(id, nome, cpf, email, mesa){
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.mesa = mesa;
+
+    }
+ }
