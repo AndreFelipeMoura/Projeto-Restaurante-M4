74d56cc779c562183b29397f446874f67a20d13b
joao pedro <joaopedrobsi10@gmail.com>
2022-07-08 19:22:55 -0300

    subindo models cozinha

---
+++ src/models/cozinha-models.js
@@ -0,0 +1,11 @@
+class cozinha{
+    constructor(nome, cpf, ingredientes, insumo,funcionarios){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.ingredientes = ingredientes;
+        this.insumo = insumo;
+        this.funcionarios = funcionarios
+    }
+}
+
+module.exports = cozinha;
