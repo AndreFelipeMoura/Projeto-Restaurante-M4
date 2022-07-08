e7e165b439b6f92fed29845c8646198edecc9fb6
Andre Moura <andre.fa.moura@outlook.com>
2022-07-08 19:32:27 -0300

    rota POST

--- src/DAO/cardapio-dao.js
+++ src/DAO/cardapio-dao.js
@@ -17,10 +17,23 @@ class cardapioDAO {
 
     listarCardapioID(id) {
         return new Promise((resolve, reject) => {
-            this.bd.all(`SELECT * FROM CARDAPIO WHERE id =${id}`, (error, resultado) => {
+            this.bd.all(`SELECT * FROM CARDAPIO WHERE id = ${id}`, (error, resultado) => {
                 if (error) res.status(404).json(error);
                 else res.status(200).json(resultado)
             })
+        })
+
+    }
+
+    insereCardapio(item) {
+        return new Promise((resolve, reject) => {
+            this.bd.run(`INSERT INTO CARDAPIO (ingredientes, pratos, bebidas, preco, numvendas)
+            VALUES (?, ?, ?, ?, ?)`,
+                [item.ingredientes, item.pratos, item.bebidas, item.preco, item.numvendas], (error) => {
+                    if (error) res.status(404).json(error);
+                    else res.status(200).json('INSERT INSERIDO')
+                })
+
         })
 
     }
--- src/controllers/cardapio-controller.js
+++ src/controllers/cardapio-controller.js
@@ -30,6 +30,20 @@ const cardapio = (app, bdsqlite) => {
         }
         data();
     })
+
+    app.post('/cardapio', (req, res) => {
+        body = req.body;
+        const novoItem = new Cardapio(body.ingredientes, body.pratos, body.bebidas, body.preco, body.numvendas)
+        const data = async () => {
+            try {
+                const cardapio = await DAOcardapio.insereCardapio(novoItem);
+                res.status(201).json(cardapio)
+            } catch (error) {
+                res.status(200).json(error)
+            }
+        }
+        data();
+    })
 }
 
 module.exports = cardapio;
