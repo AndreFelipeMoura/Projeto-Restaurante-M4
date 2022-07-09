a67b6e630bf083a833c73b0e6a7bdbbfe1a6ab86
Andre Moura <andre.fa.moura@outlook.com>
2022-07-08 21:28:23 -0300

    import controller

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {db} from "./infra/create-table.js"
+import {db} from "./infra/create-table-cardapio"
 import {clientes} from "./controllers/cliente-controller.js"
 import {garçom} from './controller/garcom-controller.js'
+import {cardapio} from './controllers/cardapio-controller'
 import express from 'express'
 const app = express()
 const port = 3000;
@@ -8,6 +10,7 @@ app.use(express.json())
 
 garçom(app)
 clientes(app, db)
+cardapio(app)
 
 app.listen(port, ()=>{
     console.log("funcionando")
