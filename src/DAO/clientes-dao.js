8414ebf08164a5186da028aa2bc980ef548cabe8
Orlando C Bernardi <orlandoceciliobernardi2017@gmail.com>
2022-07-11 17:04:28 -0300

    Projeto finalizado

--- src/DAO/clientes-dao.js
+++ src/DAO/clientes-dao.js
@@ -25,4 +25,57 @@ export class ClienteDAO{
             } )
         })
     }
+
+    async listarClientesId(id){
+        return new Promise((resolve, reject) => {
+            this.bd.all(`SELECT * FROM clientes where id = ?`, [id] , (error, result)=>{
+                if(error){
+                    reject(error);
+                }else{
+                    resolve(result)
+                }
+
+            })
+        })
+    }
+
+   async atualizarClientes(cliente, id){
+    return new Promise((resolve, reject) => {
+        this.bd.run(`UPDATE clientes set nome = ?, email = ?, cpf = ?, mesa = ? where id = ?`,[cliente.nome, cliente.email, cliente.cpf, cliente.mesa, id ],(error)=>{
+            if(error){
+                reject(error)
+            }else{
+                resolve("Cliente atualizado com sucesso!!")
+            }
+        })
+    })
+   }
+
+   async  removerClientes(id){
+    return new Promise((resolve, reject) => {
+        this.bd.run(`DElETE FROM clientes where id = ?`,id,(error)=>{
+            if (error){
+                reject(error)
+            }else{
+                resolve("Cliente deletado com sucesso!!")
+            }
+
+
+        })
+        
+    })
+   }
+
+   async  adicionarClientes(cliente){
+    return new Promise((resolve, reject) => {
+        this.bd.run(`INSERT INTO clientes (nome, cpf, email, mesa) values (?,?,?,?)` , [cliente.nome, cliente.email, cliente.cpf, cliente.mesa], (error)=>{
+            if (error){
+                reject(error)
+            }else{
+                resolve("Cliente adicionado com sucesso!!")
+            }
+        })    
+    })
+
+   }
 }
--- src/controllers/cliente-controller.js
+++ src/controllers/cliente-controller.js
@@ -25,6 +25,57 @@ export  const clientes = (app, db) => {
             console.log(error)
         }
     })
+
+    app.get("/clientes/:id",async(req,res)=>{
+        const id = req.params.id
+        try {
+            const result = await DAO.listarClientesId(id)
+            res.send(result)
+        } catch (error) {
+            console.log(error)
+            
+        }
+    } )
+    app.put("/clientes/:id", async(req,res)=>{
+        const body = req.body;
+        const id = req.params.id;
+        const clienteAntigo = await DAO.listarClientesId(id);
+        const clienteNovo = new ClienteModels(
+            body.NOME || clienteAntigo[0].NOME,
+            body.CPF|| clienteAntigo[0].CPF,
+            body.EMAIL || clienteAntigo[0].EMAIL,
+            body.MESA || clienteAntigo[0].MESA                           
+        )
+        
+        try {
+            await DAO.atualizarClientes(clienteNovo, id)
+            res.json({
+                "Dado antigo" : clienteAntigo, 
+                "Dado novo": await DAO.listarClientesId(id)
+            })
+        } catch (error) {
+            res.json({
+                "Error" : error.message 
+            })
+        }
+
+
+    })
+    app.delete("/clientes/:id", async(req,res)=>{
+        const id = req.params.id;
+
+        try {
+            const clienteDeletado = await DAO.removerClientes(id)
+            res.send("Cliente Deletado")
+
+        } catch (error) {
+            res.json({
+                "Error" : error.message 
+            })
+            
+        }
+
+    })
 }
 
 
